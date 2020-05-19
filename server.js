2e8da7d4ef55398d63537e57c55791406d469335
Michael Baska <mikebaska1@gmail.com>
2020-05-18 21:12:39 -0500

    added JawsDB information into server.js

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ var mysql = require("mysql");
 var app = express();
 var db = require("./models");
 var PORT = process.env.PORT || 3001;
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password:"yourRootPassword",
-  database: "villagers_db"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password:"yourRootPassword",
+    database: "villagers_db"
+  });
+}
 
 connection.connect(function(err){
   if (err) {
