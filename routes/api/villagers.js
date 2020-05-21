88e660794931bb00f761b1b5f7c269f047230861
Michael Baska <mikebaska1@gmail.com>
2020-05-21 11:58:28 -0500

    edited the route folder

--- routes/api/villagers.js
+++ routes/api/villagers.js
@@ -1,16 +1,8 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const villagersController = require("../../controllers/villagersController");
 
-// Matches with "/api/books"
 router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
-
-// Matches with "/api/books/:id"
-router
-  .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
+  .get(villagersController.findAll)
+  .post(villagersController.create);
 
 module.exports = router;
